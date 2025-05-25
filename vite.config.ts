eba6f72547a673cb45c1fa16d534d4ec7fcc18cf
prakasharumugam1992 <arumugam.prakash147@gmail.com>
2025-05-25 14:26:21 +0530

    inital commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/testProject/',
   build: {
     outDir: 'build' // change from 'dist' to 'build'
   },
-  base:'/testProject/',
   plugins: [react()],
 })
